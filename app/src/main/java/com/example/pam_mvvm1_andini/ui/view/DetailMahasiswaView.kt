df4d714393bdc472e6dc5ce4479d160efe6013a9
andiniseptia <andiniseptiandri124@gmail.com>
2024-11-21 17:35:58 +0700

    Menambahkan fungsi DetailMahasiswaView untuk menampilkan detail data mahasiswa

---
+++ app/src/main/java/com/example/pam_mvvm1_andini/ui/view/DetailMahasiswaView.kt
@@ -0,0 +1,22 @@
+package com.example.pam_mvvm1_andini.ui.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.pam_mvvm1_andini.model.DataMahasiswa
+
+@Composable
+fun DetailMahasiswaView(
+    modifier: Modifier = Modifier,
+    uiStateMahasiswa: DataMahasiswa,
+    onBackButton: () -> Unit
+)
