5acfc794da6a1c8220dc35cab52aa1d7a47aa89e
andiniseptia <andiniseptiandri124@gmail.com>
2024-11-21 17:48:54 +0700

    Menambahkan enum Halaman dengan nilai Formulir dan Detail untuk menentukan navigasi halaman

---
+++ app/src/main/java/com/example/pam_mvvm1_andini/PengelolaHalaman.kt
@@ -0,0 +1,21 @@
+package com.example.pam_mvvm1_andini
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pam_mvvm1_andini.model.DataJenisKelamin
+import com.example.pam_mvvm1_andini.ui.view.DetailMahasiswaView
+import com.example.pam_mvvm1_andini.ui.view.FormMahasiswaview
+import com.example.pam_mvvm1_andini.ui.viewmodel.MahasiswaViewModel
+
+enum class Halaman{
+    Formulir,
+    Detail,
+}
