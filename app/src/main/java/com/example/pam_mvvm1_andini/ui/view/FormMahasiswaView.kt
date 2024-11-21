928f8cf39efbd1e0abd329076fe567b976336588
andiniseptia <andiniseptiandri124@gmail.com>
2024-11-21 17:40:46 +0700

    Membuat fungsi FormMahasiswaview untuk form input dengan opsi jenis kelamin

---
+++ app/src/main/java/com/example/pam_mvvm1_andini/ui/view/FormMahasiswaView.kt
@@ -0,0 +1,33 @@
+package com.example.pam_mvvm1_andini.ui.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.provider.FontsContractCompat.Columns
+
+@Composable
+//Fungsi untuk form input
+fun FormMahasiswaview(
+    modifier: Modifier = Modifier,
+    listJK: List<String>,
+    onSubmitClicked: (MutableList<String>) -> Unit
+)
