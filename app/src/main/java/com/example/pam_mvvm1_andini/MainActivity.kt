33cf4852c3663d2a9ddcc937e96d2ea69e25a40d
andiniseptia <andiniseptiandri124@gmail.com>
2024-11-21 17:56:21 +0700

    Mengubah greeting menjadi pengelolahalaman

---
+++ app/src/main/java/com/example/pam_mvvm1_andini/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.pam_mvvm1_andini
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pam_mvvm1_andini.ui.theme.PAMMVVM1AndiniTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PengelolaHalaman()
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PAMMVVM1AndiniTheme {
+        Greeting("Android")
+    }
+}
