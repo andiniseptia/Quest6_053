e138732c96a656dd7bb61f98f141bad28ae8b92d
andiniseptia <andiniseptiandri124@gmail.com>
2024-11-21 17:34:12 +0700

    Membuat data class untuk variable mahasiswa

---
+++ app/src/main/java/com/example/pam_mvvm1_andini/model/DataMahasiswa.kt
@@ -0,0 +1,11 @@
+package com.example.pam_mvvm1_andini.model
+
+import android.provider.ContactsContract.CommonDataKinds.Email
+
+data class DataMahasiswa(
+    val nama: String ="",
+    val gender: String ="",
+    val email: String ="",
+    val alamat: String ="",
+    val nim: String =""
+)
