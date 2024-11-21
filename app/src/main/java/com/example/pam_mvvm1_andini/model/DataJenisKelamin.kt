c408a20e9e6cee0c1cf1be20ee8eea2f99c486a8
andiniseptia <andiniseptiandri124@gmail.com>
2024-11-21 17:33:07 +0700

    Membuat object jenis kelamin Mahasiswa

---
+++ app/src/main/java/com/example/pam_mvvm1_andini/model/DataJenisKelamin.kt
@@ -0,0 +1,10 @@
+package com.example.pam_mvvm1_andini.model
+
+import com.example.pam_mvvm1_andini.R
+
+object DataJenisKelamin {
+    val listJK = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
